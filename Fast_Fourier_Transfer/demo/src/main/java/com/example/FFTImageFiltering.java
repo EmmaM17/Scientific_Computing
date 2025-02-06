0b364ddc75b5f86d882cafc2021bc6486bbef21a
Emma McEvoy <62438729+EmmaM17@users.noreply.github.com>
2025-02-06 15:46:03 +0000

    Week 2 FFT

---
+++ Fast_Fourier_Transfer/demo/src/main/java/com/example/FFTImageFiltering.java
@@ -0,0 +1,46 @@
+package com.example;
+
+public class FFTImageFiltering {
+
+    public static int N = 256 ;
+
+    public static void main(String [] args) throws Exception {
+
+        double [] [] X = new double [N] [N] ;
+        ReadPGM.read(X, "wolf.pgm", N) ;
+
+        DisplayDensity display =
+                new DisplayDensity(X, N, "Original Image") ;
+
+        // create array for in-place FFT, and copy original data to it
+        double [] [] CRe = new double [N] [N], CIm = new double [N] [N] ;
+        for(int k = 0 ; k < N ; k++) {
+            for(int l = 0 ; l < N ; l++) {
+                CRe [k] [l] = X [k] [l] ;
+            }
+        }
+
+        fft2d(CRe, CIm, 1) ;  // Fourier transform
+
+        Display2dFT display2 =
+                new Display2dFT(CRe, CIm, N, "Discrete FT") ;
+
+        // create array for in-place inverse FFT, and copy FT to it
+        double [] [] reconRe = new double [N] [N],
+                     reconIm = new double [N] [N] ;
+        for(int k = 0 ; k < N ; k++) {
+            for(int l = 0 ; l < N ; l++) {
+                reconRe [k] [l] = CRe [k] [l] ;
+                reconIm [k] [l] = CIm [k] [l] ;
+            }
+        }
+
+        fft2d(reconRe, reconIm, -1) ;  // Inverse Fourier transform
+
+        DisplayDensity display3 =
+                new DisplayDensity(reconRe, N, "Reconstructed Image") ;
+    }
+
+    //... implementation of fft2d ...
+
+}
